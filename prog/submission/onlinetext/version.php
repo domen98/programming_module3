b800c0a4613d01a42449ac39f623cfc1180a81a4
mjekovec <mjekovec@94cf1c5c-f5da-46be-b7db-c8d3802cfb30>
2019-04-29 09:24:08 +0000

    Bump submission onlinetext version to match prog version

--- prog/submission/onlinetext/version.php
+++ prog/submission/onlinetext/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015111600;
+$plugin->version   = 2019031600;
 $plugin->requires  = 2015111000;
 $plugin->component = 'progsubmission_onlinetext';
